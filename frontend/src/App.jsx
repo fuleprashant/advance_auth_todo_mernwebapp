87c68fc5c4f8c6788b842b1e4c0638ffa68a50d0
Prashant Fule <tbs.prasanth@gmail.com>
2024-11-15 12:37:00 +0530

    add frontend react with tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-2xl bg-red-800 p-4 h-16 flex justify-center items-center">
+      App
+    </div>
+  );
+};
+
+export default App;
